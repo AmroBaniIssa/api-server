8b4a38d8343f4cd815267850d96cac476d834dac
AmroBaniIssa <amro.nedal.issa@gmail.com>
2023-06-15 07:19:14 +0000

    models

---
+++ src/routes/authors.route.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const authorsRouter = express.Router();
+const Collection = require('../models/lib/collection');
+const { AuthorModel, BookModel } = require('../models/index');
+authorsRouter.get("/author", getAllAuthors);
+authorsRouter.get("/author/:id", getOneAuthor);
+authorsRouter.post("/author", createAuthor);
+authorsRouter.put("/author/:id", updateAuthor);
+authorsRouter.delete("/author/:id", deleteAuthor);
+
+
+
+authorsRouter.get("/authorBooks/:id", authorBooks);
+async function authorBooks(req, res) {
+    const AuthorId = parseInt(req.params.id);
+    let authorBooksResult = await AuthorModel.readAuthorBooks(AuthorId, BookModel.model);
+    res.status(200).json(authorBooksResult);
+}
+async function getAllAuthors(req, res) {
+    let authorsResult = await AuthorModel.read();
+    res.status(200).json(authorsResult);
+}
+
+async function getOneAuthor(req, res) {
+    const AuthorId = parseInt(req.params.id);
+    let Author = await AuthorModel.read(AuthorId)
+    res.status(200).json(Author);
+}
+async function createAuthor(req, res) {
+    let newAuthor = req.body;
+    let Author = await AuthorModel.add(newAuthor);
+    res.status(201).json(Author);
+}
+async function updateAuthor(req, res) {
+    let AuthorId = parseInt(req.params.id);
+    let updateAuthor = req.body;
+    let foundAuthor = await AuthorModel.update(updateAuthor, AuthorId);
+    res.status(201).json(foundAuthor);
+}
+async function deleteAuthor(req, res) {
+    let AuthorId = parseInt(req.params.id);
+    let deleteAuthor = await AuthorModel.delete(AuthorId);
+    res.status(204).json(deleteAuthor);
+}
+
+module.exports = authorsRouter;
---
+++ src/routes/books.route.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const booksRouter = express.Router();
+const { BookModel } = require('../models/index');
+booksRouter.get("/book", getAllBooks);
+booksRouter.get("/book/:id", getOneBook);
+booksRouter.post("/book", createBook);
+booksRouter.put("/book/:id", updateBook);
+booksRouter.delete("/book/:id", deleteBook);
+
+async function getAllBooks(req, res) {
+    let booksResult = await BookModel.read();
+    res.status(200).json(booksResult);
+}
+
+async function getOneBook(req, res) {
+    const BookId = parseInt(req.params.id);
+    let Book = await BookModel.read(BookId)
+    res.status(200).json(Book);
+}
+async function createBook(req, res) {
+    let newBook = req.body;
+    let Book = await BookModel.add(newBook);
+    res.status(201).json(Book);
+}
+async function updateBook(req, res) {
+    let BookId = parseInt(req.params.id);
+    let updateBook = req.body;
+    let updatedBook = await foundBook.update(updateBook, BookId);
+    res.status(201).json(updatedBook);
+}
+async function deleteBook(req, res) {
+    let BookId = parseInt(req.params.id);
+    let deleteBook = await BookModel.delete(BookId);
+    res.status(204).json(deleteBook);
+}
+
+module.exports = booksRouter;
